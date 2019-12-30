147cc667899599c0d81d3766588ba6d60d38858a
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2019-12-30 09:35:17 +0100

    feat(topics): handle existing error

--- app/helpers/topics.js
+++ app/helpers/topics.js
@@ -7,10 +7,23 @@ const eventRequestHelper = require("./eventRequest");
 const listenerController = require("../controllers/listener");
 
 const create = module.exports.create = (event) => {
-	const events = _.get(event, "data.events", []);
-	const promises = _.map(events, function(e) {
-		return eventRequestHelper("PUT", "topics/" + config.name + "_" + e.topic);
-	});
+    const events = _.get(event, "data.events", []);
+
+    // check if topic already exists in eventhandler
+    const promises = _.map(events, (e) => eventRequestHelper("GET", "topics/" + config.name + "_" + e.topic)
+		.then((eventAlreadyExists) => eventAlreadyExists ? null : eventRequestHelper("POST", "topics", {
+            name: config.name + "_" + e.topic,
+        }))
+        .catch((err) => {
+            if (JSON.parse(err).code !== "NotFoundError") {
+                throw err;
+            }
+
+            return eventRequestHelper("POST", "topics", {
+                name: config.name + "_" + e.topic,
+            })
+        })
+	);
 
 	return Q.all(promises)
 		.then(() => {
@@ -35,8 +48,8 @@ const remove = module.exports.remove = (event) => {
 
 module.exports.update = (oldTopic, newTopic) => remove(oldTopic)
 	.then(
-		() => create(newTopic),
-		() => create(newTopic)
+		() => create(newTopic, true),
+		() => create(newTopic, true)
 	)
 	.then(() => {
 		listenerController.reloadConfig();
